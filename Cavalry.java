d9001770b4de74352d48e3661332ff2802186de9
swetanksng <swetanksng@gmail.com>
2016-05-21 20:06:50 -0700

    Update Cavalry.java

--- Cavalry.java
+++ Cavalry.java
@@ -1,5 +1,10 @@
 package War_Game;
 
-public class Cavalry {
+public class Cavalry extends Troop{
+
+	public Cavalry(Nation nation, String division_Name, double level, District recruit, boolean dummy) {
+		super(nation, division_Name, level, recruit, dummy);
+		// TODO Auto-generated constructor stub
+	}
 
 }
